d1cd85841c41b59df4671fa0d90e7cb4de1370e0
Chen-HuangRen <tn918419@gmail.com>
2023-09-26 06:24:37 +0800

    Add status

--- AWR1443BOOST.py
+++ AWR1443BOOST.py
@@ -1,8 +1,9 @@
 # %%
 import time
-import serial
-import serial.tools.list_ports
 import threading
+
+import serial # pyserial-3.5
+import numpy # numpy-1.26.0
 
 import Log
 import SerialTool
@@ -10,24 +11,36 @@ import Configuration
 
 # %% 
 # (Ctrl_port_name, Data_port_name, Ctrl_port_baudrate, Data_port_baudrate, Ctrl_file_name, Data_file_name)
-class AWR1443BOOST:
+class xWR14xx:
+
   Ctrl_port: serial.Serial | None = None
   Data_port: serial.Serial | None = None
+
+  State: str | None = None
+
   Config: Configuration.Configuration_2_1_0 | None = None
-  logger_config: Log.Logger | None = None
-  # logger_parse:  Log.Logger | None = None
+
+  logger: Log.Logger | None = None
+
   def __init__(self, Ctrl_port_name: str, Data_port_name: str, Ctrl_port_baudrate: int = 115200, Data_port_baudrate: int = 921600):
+
     self.Ctrl_port = serial.Serial(port=Ctrl_port_name, baudrate=Ctrl_port_baudrate)
-    self.Data_port = serial.Serial(port=Data_port_name, baudrate=Data_port_baudrate)
     SerialTool.print_serial_info(port=self.Ctrl_port, Name="Ctrl port")
+    self.Data_port = serial.Serial(port=Data_port_name, baudrate=Data_port_baudrate)
     SerialTool.print_serial_info(port=self.Data_port, Name="Data port")
-    self.logger_config = Log.Logger(fileName="config.log")
-    # self.logger_parse = Log.Logger(fileName="parse.log")
-    self.Config = Configuration.Configuration_2_1_0(platform="xWR1443")
-    
+
+    self.logger = Log.Logger(fileName="xWR14xx.log")
+
+    self.Config = Configuration.Configuration_2_1_0(platform="xWR14xx")
+
+    self.State = "initialized"
+
   def __delattr__(self, __name: str) -> None:
     self.Ctrl_port.close()
     self.Data_port.close()
+
+  def __str__(self) -> str:
+    return "xWR14xx('{Ctrl_port}', '{Data_port}', '{Ctrl_port_baudrate}', '{Data_port_baudrate}')"
 
   def configure_unit(self, command: str, wait: float | int = 0.05, echo: bool = False):
     """unit configuration
@@ -38,10 +51,19 @@ class AWR1443BOOST:
       echo (bool, optional): Display configuration instructions. Defaults to False.
     """
     self.Ctrl_port.write(command.rstrip('\r\n').__add__('\n').encode())
-    self.logger_config.log(command)
-    self.Config.parse_commandParameters_1443(command)
+    self.Config.parse_commandParameters_1443(command=command)
+
+    self.logger.log(event="config", level="logging", message="command: `{}`".format(command))
     if echo: print(command.rstrip('\n').rstrip('\r'))
+
+    command_main: str = command.split(' ')[0]
+    if command_main == "sensorStart":
+      self.State = "Sensor_Start"
+    if command_main == "sensorStop":
+      self.State = "Sensor_Stop"
+    
     time.sleep(wait)
+
   def configure(self, commands: list[str] | str, wait: float | int = 0.05, echo: bool = False):
     """configuration
     Args:
@@ -65,12 +87,10 @@ class AWR1443BOOST:
     with open(file=CFG_file_name, mode="r") as CFG_file:
       CFG_lines: list[str] = [CFG_line.rstrip('\r\n').__add__('\n') for CFG_line in CFG_file.readlines()]
       self.configure(commands=CFG_lines, wait=wait, echo=echo)
-    for cfg_key in self.Config.commandParameters.keys():
-      print(cfg_key)
       
 # %%
 if __name__ == '__main__':
-  device = AWR1443BOOST(
+  device = xWR14xx(
     Ctrl_port_name="COM3", 
     Data_port_name="COM4", 
     Ctrl_port_baudrate=115200, 
