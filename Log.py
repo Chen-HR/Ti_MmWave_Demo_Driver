21670299b575de7ef9cf9ad2527b88881da400d5
Chen-HuangRen <tn918419@gmail.com>
2023-10-03 03:04:35 +0800

    Fix recording time error

--- Log.py
+++ Log.py
@@ -4,13 +4,12 @@ formatted_time = lambda timezone, format: datetime.datetime.now(tz=timezone).str
 
 class Logger:
 
-  def __init__(self, fileName: str, prefix = formatted_time(datetime.timezone.utc, "%Y-%m-%d %H:%M:%S.%f")) -> None:
+  def __init__(self, fileName: str) -> None:
     self.fileName = fileName
-    self.prefix: str = prefix
 
   def log(self, event: str, level: str, message: str):
     with open(file=self.fileName, mode='a+') as file:
-      file.write("[{prefix}] <{event}> ({level}) : {message}\n".format(prefix=self.prefix, event=event, level=level, message=message))
+      file.write("[{prefix}] <{event}> ({level}) : {message}\n".format(prefix=formatted_time(datetime.timezone.utc, "%Y-%m-%d %H:%M:%S.%f"), event=event, level=level, message=message))
 
 if __name__ == '__main__':
   LoggerTester = Logger(fileName="LoggerTester.log")
