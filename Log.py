98bc252e7cb6b8635ce8999ab0d40ec15240b762
Chen-HuangRen <tn918419@gmail.com>
2023-10-12 07:20:48 +0800

    Supports simultaneous display on standard output

--- Log.py
+++ Log.py
@@ -4,12 +4,15 @@ formatted_time = lambda timezone, format: datetime.datetime.now(tz=timezone).str
 
 class Logger:
 
-  def __init__(self, fileName: str) -> None:
+  def __init__(self, fileName: str, echo: bool = False) -> None:
     self.fileName = fileName
+    self.echo = echo
 
   def log(self, event: str, level: str, message: str):
     with open(file=self.fileName, mode='a+') as file:
-      file.write("[{prefix}] <{event}> ({level}) : {message}\n".format(prefix=formatted_time(datetime.timezone.utc, "%Y-%m-%d %H:%M:%S.%f"), event=event, level=level, message=message))
+      output = "[{prefix}] <{event}> ({level}) : {message}".format(prefix=formatted_time(datetime.timezone.utc, "%Y-%m-%d %H:%M:%S.%f"), event=event, level=level, message=message)
+      file.write(output + '\n')
+      if self.echo: print(output)
 
 if __name__ == '__main__':
   LoggerTester = Logger(fileName="LoggerTester.log")
